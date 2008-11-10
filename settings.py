6e3e9ea503b08011f5e849eb108eecec664176cc
Trey Piepmeier <trey@treypiepmeier.com>
2008-11-10 17:44:52 -0600

    Update settings.

--- settings.py
+++ settings.py
@@ -67,11 +67,10 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'chiplog_proj.urls'
 
+import os.path
+
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    # '/Users/trey/Sites/dev/Django/chiplog_proj/templates',
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 INSTALLED_APPS = (
