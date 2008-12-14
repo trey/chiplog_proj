d618e6c0b15dd0250cb6f5b1b6bb98b5da56e44b
Trey Piepmeier <trey@treypiepmeier.com>
2008-12-14 17:51:36 -0600

    More project-level admin customization.

--- admin.py
+++ admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.redirects.models import Redirect
+from tagging.models import Tag, TaggedItem
 from chiplog.models import Entry
 from chiplog.admin import EntryAdmin
 
@@ -8,3 +10,6 @@ class AdminSite(admin.AdminSite):
 site = AdminSite()
 
 site.register(Entry, EntryAdmin)
+site.register(Redirect)
+site.register(Tag)
+site.register(TaggedItem)
